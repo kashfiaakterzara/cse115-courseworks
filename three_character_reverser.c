dd10821a8ef404c471704379f8698d4e8a651ba1
kashfiaakterzara <kashfiaakter13@gmail.com>
2026-06-27 04:54:38 +0600

    Add files via upload

---
+++ three_character_reverser.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+ char c1, c2, c3;
+
+ printf("Enter three characters: ");
+ scanf("%c%c%c", &c1,&c2,&c3);
+
+ printf("Reverse: %c%c%c", c3, c2, c1);
+
+
+    return 0;
+}
